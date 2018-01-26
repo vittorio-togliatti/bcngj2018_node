4743ed34496d6a2fdf7d8102379959f28ed0f450
Vittorio <vienianchetu@gmail.com>
2018-01-27 00:17:39 +0100

    primera subida

---
+++ api/models/todoListModel.js
@@ -0,0 +1,29 @@
+
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var TaskSchema = new Schema({
+  name: {
+    type: String,
+    required: 'Kindly enter the name of the task'
+  },
+json_id: {
+    type: String,
+    default: '123'
+  },
+  Created_date: {
+    type: Date,
+    default: Date.now
+  },
+  status: {
+    type: [{
+      type: String,
+      enum: ['pending', 'ongoing', 'completed']
+    }],
+    default: ['pending']
+  }
+});
+
+module.exports = mongoose.model('Tasks', TaskSchema);
